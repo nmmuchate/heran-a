50583ed60961fab72099c15cef16d7d681bb7411
alfredofrancisco <alxerindza@gmail.com>
2019-12-20 08:29:38 +0200

    adição dos atributos e do construtores

---
+++ src/herança/Pessoa.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herança;
+
+/**
+ *
+ * @author Vagabundo
+ */
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa() {
+    }
+    
+}
