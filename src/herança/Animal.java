bda9ed278b251644c022c9bc01759dc99cbbb797
alfredofrancisco <alxerindza@gmail.com>
2019-12-20 07:48:09 +0200

    Initial commit

---
+++ src/herança/Animal.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herança;
+
+/**
+ *
+ * @author Vagabundo
+ */
+public class Animal {
+    private String raca;
+    private String nomedoanimal;
+    
+    public Animal(){
+        
+    }
+    public Animal (String nome){
+        
+    }
+
+    /**
+     * @return the raca
+     */
+    public String getRaca() {
+        return raca;
+    }
+
+    /**
+     * @param raca the raca to set
+     */
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    /**
+     * @return the nomedoanimal
+     */
+    public String getNomedoanimal() {
+        return nomedoanimal;
+    }
+
+    /**
+     * @param nomedoanimal the nomedoanimal to set
+     */
+    public void setNomedoanimal(String nomedoanimal) {
+        this.nomedoanimal = nomedoanimal;
+    }
+    
+          
+    
+}
