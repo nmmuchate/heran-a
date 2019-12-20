6d40910f5797974618cd69898ae08dbf01dbecd4
alfredofrancisco <alxerindza@gmail.com>
2019-12-20 08:45:47 +0200

    classe miseravel-metodo void

---
+++ src/herança/Miseravel.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herança;
+
+/**
+ *
+ * @author Vagabundo
+ */
+public class Miseravel extends Pessoa{
+    public void mendigar(){
+        
+    }
+
+    public Miseravel(int  aum) {
+        aum =0;
+        aum=aum+aum;
+    }
+    
+}
