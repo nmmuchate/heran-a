6fdc8e8746e6ffd1c8f4adad4c2ee4f0191f725b
alfredofrancisco <alxerindza@gmail.com>
2019-12-20 08:49:30 +0200

    classe herança metodo main

---
+++ src/herança/Pobre.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herança;
+
+/**
+ *
+ * @author Vagabundo
+ */
+public class Pobre extends Pessoa{
+    public void trabalha(){
+        System.out.println("sou trabalhador");
+    }
+    
+    
+}
